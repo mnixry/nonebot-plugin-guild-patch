b6cd7240811dae43cddc625e0516a7454598fa0c
Mix <mnixry@users.noreply.github.com>
2022-03-06 23:31:59 +0800

    :art: Improve message append

--- nonebot_plugin_guild_patch/__init__.py
+++ nonebot_plugin_guild_patch/__init__.py
@@ -7,7 +7,10 @@ original_send = Bot.send
 
 
 async def patched_send(
-    self: Bot, event: Event, message: Union[Message, MessageSegment, str], **kwargs
+    self: Bot,
+    event: Event,
+    message: Union[Message, MessageSegment, str],
+    **kwargs,
 ):
     guild_id: Optional[int] = getattr(event, "guild_id", None)
     channel_id: Optional[int] = getattr(event, "channel_id", None)
@@ -24,12 +27,16 @@ async def patched_send(
         escape(message, escape_comma=False) if isinstance(message, str) else message
     )
 
-    message_sent = message if isinstance(message, Message) else Message(message)
+    message_sent = Message()
     if user_id and kwargs.get("at_sender", False):
-        message_sent = MessageSegment.at(user_id) + " " + message_sent
+        message_sent += MessageSegment.at(user_id) + " "
+    message_sent += message
 
     return await self.send_guild_channel_msg(
-        guild_id=guild_id, channel_id=channel_id, message=message_sent
+        guild_id=guild_id,
+        channel_id=channel_id,
+        message=message_sent,
+        **kwargs,
     )
 
 
